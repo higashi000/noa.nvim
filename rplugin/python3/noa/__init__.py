10047ac8ce2d1494279660dd02a06cda8ec604b6
higashi000 <higashi.ict@gmail.com>
2020-06-11 18:49:15 +0900

    Add websocket

--- rplugin/python3/noa/__init__.py
+++ rplugin/python3/noa/__init__.py
@@ -1,11 +1,30 @@
 import neovim
 import random
+import websocket
 
 @neovim.plugin
 class Noa(object):
 
     def __init__(self, nvim):
+        websocket.enableTrace(True)
         self.nvim = nvim
+        self.ws = websocket.create_connection("ws://localhost:5000/ws")
+
+    def __del__(self):
+        self.ws.close()
+
+    @neovim.command('NoaSend', nargs = '*')
+    def NoaWSSend(self, args):
+        if (len(args) > 0):
+            self.ws.send(args[0])
+            result = self.ws.recv()
+            self.nvim.command('echo "%s"' % result)
+
+    @neovim.function('NoaWSRECV', sync = False)
+    def NoaWSRECV(self, args):
+        while True:
+            result = self.ws.recv()
+            self.nvim.command('echo "%s"' % result)
 
     @neovim.function('NoaTestFunction', sync=False)
     def NoaTestFunc(self, args):
